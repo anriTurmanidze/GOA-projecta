db194d50f86703f3593631912bf7aa0024217d63
anriTurmanidze <anri.turmanidze03@mail.ru>
2024-05-05 01:13:52 +0400

    first commit

---
+++ day 01/homework/lesson1_homework.py
@@ -0,0 +1,8 @@
+name = "Anri"
+surname = "Turmanidze"
+age = 21
+height = 1.81
+university = "business and technology university"
+
+
+print("hello, my name is " + name + " " + surname + ", I am  " + str(age) + " years old and my height is "  + str(height) + " cm, also i'm studying at "  + university)
