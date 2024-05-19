99144a960a0ce16490e675334b2aae95b3e158e3
anriTurmanidze <anri.turmanidze03@mail.ru>
2024-05-19 13:49:26 +0400

    commit

---
+++ level 04/classwork/main.py
@@ -0,0 +1,9 @@
+# x = input("please choose a number: ")
+# y = input("please choose a number: ")
+
+# if x > y :
+#     print(x + " is higher number than " + y)
+# elif x < y :
+#     print(y + " is higher number than " + x)
+# else:
+#     print(y + " is equal to " + x)
---
+++ level 04/homework/lesson4.py
@@ -0,0 +1,42 @@
+#1.
+price = 3.99
+quantity = 10
+pay = price * quantity
+print(int(pay))
+print(float(pay))
+
+#2.
+
+me = 21
+mom = 60
+add_10_years_to_me = me + 10 
+add_10_years_to_mom = mom + 10
+print(add_10_years_to_me)
+print(add_10_years_to_mom)
+
+
+#3.
+
+x = 2
+y = 5
+print(x * y)
+print(x + y)
+print(x / y)
+print(x - y)
+
+
+#4.
+name = "anri"
+print(name)
+age = 21
+print(age)
+
+
+#5.
+
+me = 21
+mom = 60
+add_10_years_to_me = me + 10 
+add_10_years_to_mom = mom + 10
+divide = add_10_years_to_mom / add_10_years_to_me
+print(divide)
