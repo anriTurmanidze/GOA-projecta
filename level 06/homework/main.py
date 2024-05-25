b57a7dfe385994444bf6cd4120499d45d8997183
anriTurmanidze <anri.turmanidze03@mail.ru>
2024-05-25 15:07:57 +0400

    commit

---
+++ level 06/homework/main.py
@@ -0,0 +1,33 @@
+#1
+name = input("please input your name: ")
+surname = input("please input your surname: ")
+print("Hello, my name is " + name + " " + surname)
+
+
+#2
+x = 5
+y = input("please input number: ")
+print(x + int(y))
+
+#3
+x = int(input("please input number: "))
+y = int(input("please input number: "))
+z = int(input("please input number: "))
+print((x + y + z ) / 3 )
+
+
+#4
+x = input("please input number: ")
+print(int(x) * 5)
+y = int(input("please input number: "))
+print(y * 5)
+
+#5
+
+x = int(input("please input number: "))
+y = int(input("please input number: "))
+
+print(x * y)
+print(x / y)
+print(x + y)
+print(x - y)
