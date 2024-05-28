32d878eff39c89be6270ce664da499b0d536e051
anriTurmanidze <anri.turmanidze03@mail.ru>
2024-05-28 18:28:28 +0400

    commit

---
+++ level 07/homework/main.py
@@ -0,0 +1,26 @@
+#1
+print(5 + int("5"))
+print(5 + "5")
+print(str(5) + "5")
+print(str(5) + 5)
+print(5 + float("5"))
+print("5" + float(5))
+int(print(5 + "5"))
+
+
+
+#2შემოვატანინოთ მომხმარებელს 5 სხვადასხვა რიცხვი
+x = int(input("please input number: "))
+y = int(input("please input number: "))
+z = int(input("please input number: "))
+a = int(input("please input number: "))
+b = int(input("please input number: "))
+#დავბეჭდოთ მომხმარებლის მიერ შემოტანილი რიცხვების მთელი განაყოფი და ნაშთები
+print(a // b)
+print(a // y)
+print(a // x)
+print(a // z)
+print(a % y)
+print(a % x)
+print(a % b)
+print(a % z)
---
+++ level 08/homework/main.py
@@ -0,0 +1,60 @@
+#1
+# print(5 > 5)
+# print(7 > 5)
+# print(2 > 5)
+# print(4 > 5)
+# print(10 > 5)
+
+
+# print(5 < 5)
+# print(7 < 5)
+# print(2 < 5)
+# print(4 < 5)
+# print(10 < 5)
+
+
+# print(5 >= 5)
+# print(7 >= 5)
+# print(2 >= 5)
+# print(4 >= 5)
+# print(10 >= 5)
+
+
+# print(5 <= 5)
+# print(7 <= 5)
+# print(2 <= 5)
+# print(4 <= 5)
+# print(10 <= 5)
+
+
+# print(5 == 5)
+# print(7 == 5)
+# print(2 == 5)
+# print(4 == 5)
+# print(10 == 5)
+
+
+# print(5 != 5)
+# print(7 != 5)
+# print(2 != 5)
+# print(4 != 5)
+# print(10 != 5)
+
+
+#2
+
+# print(5 > 6 and True)
+# print(5 > 6 and False)
+# print(5 > 4 and False)
+# print(5 > 3 and True)
+# print(10 > 6 and False)
+# print(7 > 6 and True)
+
+
+
+# print(5 > 6 or True)
+# print(5 > 6 or True)
+# print(5 > 4 or False)
+# print(5 > 9 or False)
+# print(10 > 6 or False)
+# print(7 > 9 or False)
